42452238bfa4fd490aeaf08dff370145974d7f0b
Collin Watson <collinwat@gmail.com>
2012-10-22 14:31:56 -0700

    Removed trailing slash

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ if py > (2, 7) or py < (2, 7):
 
 
 # Specify non-pypi dependency links (i.e. locate poold.in packages)
-pooldin = 'http://%s:%s@code.poold.in/pypi/%s/' % (username, password, '%s')
+pooldin = 'http://%s:%s@code.poold.in/pypi/%s' % (username, password, '%s')
 
 links = [
     pooldin % 'pooldwww/#egg==pooldwww',
