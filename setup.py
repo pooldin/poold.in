be7d965a0076f74a2a9ad154f904e05c12063539
Collin Watson <collinwat@gmail.com>
2012-10-22 19:54:58 -0700

    Test

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ if py > (2, 7) or py < (2, 7):
 
 # Specify non-pypi dependency links (i.e. locate poold.in packages)
 pooldin = 'http://%s:%s@code.poold.in/pypi/%s' % (username, password, '%s')
+
 links = [
     pooldin % 'pooldwww/#egg==pooldwww',
 ]
